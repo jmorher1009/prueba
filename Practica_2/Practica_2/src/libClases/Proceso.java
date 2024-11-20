7118ab278d1eda6c0bb3cdc0416d91f667391ead
chema <72257361+jmorher1009@users.noreply.github.com>
2024-11-20 15:37:59 +0100

    Add files via upload

---
+++ Practica_2/Practica_2/src/libClases/Proceso.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package libClases;
+
+public interface Proceso {
+
+    public abstract boolean equals(Object obj); //true sin son iguales
+
+    void ver(); //muestra en pantalla el objeto
+}
